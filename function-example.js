1399b4c12c258d76085fd4201a2fd245d7766772
AlexxJoelCDS <20213tn019@utez.edu.mx>
2024-05-13 11:42:49 -0600

    Fix typo in example-errors.txt and update enterOnlyNumbers function in function-example.js

--- function-example.js
+++ function-example.js
@@ -1,4 +1,8 @@
-export const enterOnlyNumbers = (event: KeyboardEvent): boolean => {
+/*
+* @Description: This file contains the example of functions in javascript.
+*/
+
+export const enterOnlyNumbers = (event) => {
     const valueAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '8', '9'];
     const keysAllowed = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Tab', 'Enter', 'Escape', 'Delete', 'Shift', 'Control', 'Alt'];
     if (valueAllowed.includes(event.key) || keysAllowed.includes(event.key)) {
