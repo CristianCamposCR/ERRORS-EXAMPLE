64ee5c4575271798f585936c7e8d58f4304156c9
Cristian Campos <i20213tn003@utez.edu.mx>
2024-05-13 11:53:14 -0600

    [UPDATE] cambios en la funcion por cristian

--- function-example.js
+++ function-example.js
@@ -1,9 +1,13 @@
-export const enterOnlyNumbers = (event: KeyboardEvent): boolean => {
+export const enterOnlyNumbers = (event) => {
     const valueAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '8', '9'];
     const keysAllowed = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Tab', 'Enter', 'Escape', 'Delete', 'Shift', 'Control', 'Alt'];
     if (valueAllowed.includes(event.key) || keysAllowed.includes(event.key)) {
         return true;
     } else {
+        // adding some lines
+        console.log("Cambios inesperados")
+        const value = "Soy nuevo"
+        console.log("Este es el valor: ", value)
         event.preventDefault();
         return false;
     }
